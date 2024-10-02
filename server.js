7a8697f4754d3d6f6db6106dbd2f5c4128b41fb6
KillerQueen59 <fauzanramadhan59@gmail.com>
2024-10-02 12:02:54 +0700

    change server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "10.99.1.201";
 const port = process.env.port || 8080;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
